ba8ccee4a01b9502297fe4d4e30e18c9542a3df8
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-02-17 21:48:57 +0200

    test_commit_one

---
+++ test_folder/shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "main.h"
+
+/* calls the prompt function only*/
+int main(void)
+{
+	prompt();
+	return (0);
+}
