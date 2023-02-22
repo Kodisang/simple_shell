ba3682cd5b2e0cc8007923f9367ecb366f6cb3c2
Kodisang <br.kodisang@gmail.com>
2023-02-22 03:56:42 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,72 +1,54 @@
 #include "shell.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-/**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- * Return: 1 if interactive mode, 0 otherwise
- */
-int interactive(info_t *info)
+int main(void)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+    char *line;
+    char *argv[100];
+    size_t len = 0;
+    pid_t pid;
+    int status;
+
+    while (1) {
+        printf("$ ");
+        getline(&line, &len, stdin);
+
+        int i = 0;
+        argv[i] = strtok(line, " \n");
+
+        while (argv[i] != NULL) {
+            i++;
+            argv[i] = strtok(NULL, " \n");
+        }
+
+        if (argv[0] == NULL) {
+            continue;
+        }
+
+        if (strcmp(argv[0], "exit") == 0) {
+            exit(0);
+        }
+
+        pid = fork();
+
+        if (pid == 0) {
+            if (execve(argv[0], argv, NULL) == -1) {
+                perror(argv[0]);
+            }
+
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            perror("fork");
+        } else {
+            do {
+                waitpid(pid, &status, WUNTRACED);
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    }
+
+    return (0);
 }
 
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-
-/**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
- */
-
-int _atoi(char *s)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-		{
-			if (s[i] == '-')
-				sign *= -1;
-
-			if (s[i] >= '0' && s[i] <= '9')
-				{
-					flag = 1;
-					result *= 10;
-					result += (s[i] - '0');
-					}
-			else if (flag == 1)
-				flag = 2;
-		}
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
---
+++ shellAlfred
@@ -0,0 +1,16 @@
+/*
+ * File: shell.c
+ * Auth: Alfredo Kambasha
+ */
+
+#include "main.h"
+
+/**
+ * main - brings up the prompt.
+ * Return: 0 on success.
+*/
+int main(void)
+{
+	prompt();
+	return (0);
+}
