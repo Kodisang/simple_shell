3d5171db24529a41bb6e6940e7abea05bcc3fa28
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-02-23 11:53:34 +0200

    fixed errors + added code

--- rename_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git remote set-url origin https://github.com/Sean/Simple-shell.git
-git pull
-mv Simple-shell simple_shell
-git add .
-git commit -m 'Rename repository from Simple-shell to simple_shell'
-git push
--- test_folder/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-char *prompt(void);
-int _execve(char *to_exec);
-
-#endif
